c642c0361452d0794936471e1faefbb02db18eae
Edward Thomson <ethomson@edwardthomson.com>
2018-03-19 20:10:31 +0000

    reader: a generic way to read files from repos
    Similar to the `git_iterator` interface, the `git_reader` interface will
    allow us to read file contents from an arbitrary repository-backed data
    source (trees, index, or working directory).

---
+++ src/reader.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+#ifndef INCLUDE_reader_h__
+#define INCLUDE_reader_h__
+
+#include "common.h"
+
+typedef struct git_reader git_reader;
+
+struct git_reader {
+	int (*read)(git_buf *out, git_reader *reader, const char *filename);
+	void (*free)(git_reader *reader);
+};
+
+extern int git_reader_for_nothing(
+	git_reader **out);
+
+extern int git_reader_for_tree(
+	git_reader **out,
+	git_tree *tree);
+
+extern int git_reader_for_index(
+	git_reader **out,
+	git_repository *repo,
+	git_index *index);
+
+extern int git_reader_for_workdir(
+	git_reader **out,
+	git_repository *repo);
+
+extern int git_reader_read(
+	git_buf *out,
+	git_reader *reader,
+	const char *filename);
+
+extern void git_reader_free(git_reader *reader);
+
+#endif
